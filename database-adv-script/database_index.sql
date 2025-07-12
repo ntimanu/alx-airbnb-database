2d513070e5a378fd6d0009f243bb3a153133eb0a
ntimanu <ntihinyukae@gmail.com>
2025-07-12 16:38:38 +0200

    Add EXPLAIN ANALYZE to measure performance before and after indexing

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,15 +1,33 @@
--- Indexes to improve performance of joins and filters
+-- Analyze performance BEFORE indexing
+EXPLAIN ANALYZE
+SELECT
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id)
+FROM
+    users u
+JOIN
+    bookings b ON u.user_id = b.user_id
+GROUP BY
+    u.user_id, u.first_name, u.last_name;
 
--- User table
+-- Create indexes
 CREATE INDEX idx_users_user_id ON users(user_id);
-
--- Bookings table
 CREATE INDEX idx_bookings_user_id ON bookings(user_id);
 CREATE INDEX idx_bookings_property_id ON bookings(property_id);
 CREATE INDEX idx_bookings_created_at ON bookings(created_at);
-
--- Properties table
 CREATE INDEX idx_properties_property_id ON properties(property_id);
-
--- Reviews table
 CREATE INDEX idx_reviews_property_id ON reviews(property_id);
+
+-- Analyze performance AFTER indexing
+EXPLAIN ANALYZE
+SELECT
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id)
+FROM
+    users u
+JOIN
+    bookings b ON u.user_id = b.user_id
+GROUP BY
+    u.user_id, u.first_name, u.last_name;
